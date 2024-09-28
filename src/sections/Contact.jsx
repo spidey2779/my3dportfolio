e4e4bda11cf77040536625f16b9678a00ef2bada
spidey2779 <gteja2779@gmail.com>
2024-09-29 00:21:31 +0530

    change made

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
 
 
   return (
-    <section className="c-space my-20 " id="contact">
+    <section className="c-space my-28 " id="contact">
       {/* {alert.show && <Alert {...alert} />} */}
 
       <div className="relative min-h-screen flex items-center justify-center flex-col">
